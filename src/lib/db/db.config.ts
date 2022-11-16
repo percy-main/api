5b9eb121badf3f8aed5cbc2b148fb21386579a6b
Alex Young <alex.young@clinchd.com>
2022-11-16 22:46:25 +0000

    refactor: 💡 switch to database URL for ease of fly env

--- src/lib/db/db.config.ts
+++ src/lib/db/db.config.ts
@@ -3,14 +3,7 @@ import { buildConfig, Config } from "../config";
 export const DB_CONFIG = Symbol("DB_CONFIG");
 
 export const dbConfig = buildConfig((b) => ({
-  host: b.required("DB_HOST"),
-  user: b.required("DB_USER"),
-  password: b.required("DB_PASSWORD"),
-  database: b.required("DB_DATABASE"),
-  port: b.required("DB_PORT", {
-    coerce: b.coerce.number,
-    validate: b.validate.number,
-  }),
+  connectionString: b.required("DATABASE_URL"),
 }));
 
 export type DbConfig = Config<typeof dbConfig>;
--- src/lib/db/db.provider.ts
+++ src/lib/db/db.provider.ts
@@ -4,13 +4,9 @@ import { DbConfig, DB_CONFIG } from "./db.config";
 export const dbClientProvider = {
   inject: [DB_CONFIG],
   provide: Client,
-  useFactory: async ({ host, user, password, database, port }: DbConfig) => {
+  useFactory: async ({ connectionString }: DbConfig) => {
     const client = new Client({
-      host,
-      user,
-      password,
-      database,
-      port,
+      connectionString,
     });
 
     await client.connect();
