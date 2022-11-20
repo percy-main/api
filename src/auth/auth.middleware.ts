e57978fc2a3407a8ce9c601aaeff99ec8c07d225
Alex Young <alex.young@clinchd.com>
2022-11-20 23:57:47 +0000

    refactor: 💡 remove console log

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -5,8 +5,6 @@ import { middleware } from "supertokens-node/framework/express";
 @Injectable()
 export class AuthMiddleware implements NestMiddleware<Request, Response> {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log("Auth middleware");
-
     return middleware()(req, res, next);
   }
 }
