3375e17f234987dbdfc6f89fda3b3874c0ce4b40
Alex Young <alex.young@clinchd.com>
2022-11-28 22:08:04 +0000

    feat: 🎸 membership scaffold

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { APP_INTERCEPTOR } from "@nestjs/core";
 import { AuthModule } from "./auth/auth.module";
 import { LogInterceptor } from "./lib/logging/log.interceptor";
 import { LogModule } from "./lib/logging/log.module";
+import { MembershipModule } from "./membership/membership.module";
 import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [LogModule, AuthModule, UserModule],
+  imports: [LogModule, AuthModule, UserModule, MembershipModule],
   providers: [
     {
       provide: APP_INTERCEPTOR,
